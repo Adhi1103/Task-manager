a5099d699211b48f5e572f0935e421c4b75de51b
Adhi1103 <guptaadhikansh83@gmail.com>
2026-06-01 17:56:05 +0530

    Update taskService.js

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://task-manager-688r.onrender.com";
 
 export const getTasks = () => axios.get(API_URL);
 
